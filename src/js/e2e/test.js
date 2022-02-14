84cf3aa739487ac5d6b58b5ab486a373e04972e9
Nikita <dragon768@mail.ru>
2022-02-14 20:01:54 +0500

    Puppeteer

--- src/js/e2e/test.js
+++ src/js/e2e/test.js
@@ -6,11 +6,12 @@ describe('INN/ORGN from', () => {
   let page = null;
   const baseUrl = 'http://localhost:8888';
   beforeAll(async () => {
-    browser = await puppeteer.launch({
+    browser = await puppeteer.launch(
+    /*{
       headless: false,
       slowMo: 50,
       devtools: true,
-    });
+    }*/);
     page = await browser.newPage();
   });
   afterAll(async () => {
